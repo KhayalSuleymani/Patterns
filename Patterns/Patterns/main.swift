d407014be0bc37ddc986326adf8b156df83a1c56
ABA IT <xcode_man@icloud.com>
2018-08-27 15:36:43 +0400

    Initial commit

---
+++ Patterns/Patterns/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Patterns
+//
+//  Created by ABA IT on 8/27/18.
+//  Copyright © 2018 ABA IT. All rights reserved.
+//
+
+import Foundation
+// Begin understanding Design Patterns using pattern templates
+
+
+
+
